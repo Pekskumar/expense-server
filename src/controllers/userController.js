c4837dd17ac9b885e3d8e88109112f3d17a39f10
Pekskumar <79084617+Pekskumar@users.noreply.github.com>
2024-07-03 21:44:05 +0530

    modal name change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../Models/User");
 const { getResponse } = require("../utils/utils");
 const bcrypt = require("bcryptjs");
 
